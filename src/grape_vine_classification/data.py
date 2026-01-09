8b7af26081cac8bf1257107ec36c1adb0dfc8e2f
karluc <s204354@dtu.dk>
2026-01-09 14:04:07 +0100

    Cleaned up data preprocessing code

--- src/grape_vine_classification/data.py
+++ src/grape_vine_classification/data.py
@@ -1,54 +1,32 @@
 from pathlib import Path
-from torch.utils.data import DataLoader, Dataset
-import torchvision.transforms as T
-import typer
-from torch.utils.data import Dataset, random_split
-from torch import manual_seed, save
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
 import os
+import torch
+from torch.utils.data import random_split
+from torchvision import datasets, transforms
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def preprocess():
+    torch.manual_seed(1)
+    base_dir = Path(__file__).parent.parent.parent
+    raw_data_dir = base_dir / "data" / "Grapevine_Leaves_Image_Dataset"
+    processed_data_dir = base_dir / "data" / "processed_dataset"
 
-    def __init__(self, data_path: Path) -> None:
-        self.data_path = data_path
-
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
-
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
-
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
-
-
-
-def preprocess(raw_dir, processed_dir):
-    manual_seed(1)
-    # Preprocess the data
-
-    transform = T.Compose([
+    # transform images
+    transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),
-    T.ToTensor(),])
-    # Load images
-    # base_dir
+    transforms.ToTensor(),])
 
-    full_dataset = datasets.ImageFolder(root=raw_dir, transform=transform)
+    # load images (each subfolder is interpreted as a class)
+    full_dataset = datasets.ImageFolder(root=raw_data_dir, transform=transform)
 
+    # split dataset
     train_size = int(0.8 * len(full_dataset))
     test_size = len(full_dataset) - train_size
-
     train_dataset, test_dataset = random_split(full_dataset, [train_size, test_size])
 
-
-    if not os.path.exists(processed_dir):
-        os.makedirs(processed_dir)
-    save(train_dataset,f"{processed_dir}/train_data.pt")
-    save(test_dataset,f"{processed_dir}/test_data.pt")
-
-
+    # save test and train datasets
+    os.makedirs(processed_data_dir, exist_ok=True)
+    torch.save(train_dataset,f"{processed_data_dir}/train_data.pt")
+    torch.save(test_dataset,f"{processed_data_dir}/test_data.pt")
 
 if __name__ == "__main__":
-    preprocess("data/raw_data//Grapevine_Leaves_Image_Dataset","data/proccesed_data/")
+    preprocess()
--- src/grape_vine_classification/import_data.py
+++ src/grape_vine_classification/import_data.py
@@ -1,22 +1,24 @@
 import kagglehub
 import shutil
 import os
+from pathlib import Path
 
 # 1. Download to default cache
 cache_path = kagglehub.dataset_download("muratkokludataset/grapevine-leaves-image-dataset")
 
 # 2. Define your target folder
-target_folder = "./data/raw_data"
-os.makedirs(target_folder, exist_ok=True)
+base_dir = Path(__file__).parent.parent.parent
+target_path = base_dir / "data"
+os.makedirs(target_path, exist_ok=True)
 
 # 3. Move files from cache to your folder
 # Note: Use copy if you want to keep a backup in cache, or move to save space
 for item in os.listdir(cache_path):
     s = os.path.join(cache_path, item)
-    d = os.path.join(target_folder, item)
+    d = os.path.join(target_path, item)
     if os.path.isdir(s):
         shutil.copytree(s, d, dirs_exist_ok=True)
     else:
         shutil.copy2(s, d)
 
-print(f"Data is now available in: {target_folder}")
+print(f"Data is now available in: {target_path}")
