fec5d2ebb5cef6d2f0b1e2b9d4e60f98e9291a7e
karluc <s204354@dtu.dk>
2026-01-16 13:46:52 +0100

    Restructured train_lightning.py - moved sweep functionality to sweep.py

---
+++ src/grape_vine_classification/sweep.py
@@ -0,0 +1,32 @@
+import wandb
+from grape_vine_classification.train_lightning import train
+from pathlib import Path
+import typer
+import yaml
+from pytorch_lightning.loggers import WandbLogger
+
+def sweep_train():
+    wandb.init()
+    config = wandb.config
+    logger = WandbLogger()
+    train(config, logger = logger)
+
+def main(config_path: str = "configs/sweep1_lightning.yaml", project_name: str = "sweeps"):
+    path = Path(config_path)
+    if path.exists():
+        with open(path, 'r') as f:
+            sweep_config = yaml.safe_load(f)
+    else:
+        raise RuntimeError("The config path is not valid")
+
+    sweep_id = wandb.sweep(
+        sweep_config,
+        project = project_name,
+        entity = "Burnsides_Bitches"
+    )
+
+    wandb.agent(sweep_id, function = sweep_train)
+
+if __name__ == "__main__":
+    typer.run(main)
+    
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,9 @@
 import torch
 from src.grape_vine_classification.train_lightning import train
 import os
-from tests import PATH_MODEL, model_config_path
+from tests import PATH_MODEL, model_config
 
 def test_train():
-    train(model_config_path, model_name = "test_model.pth", logger = False)
+    train(model_config, output_model_name = "test_model.pth", logger = False)
 
     assert os.path.isfile(PATH_MODEL / "test_model.pth"), "No model saved"
