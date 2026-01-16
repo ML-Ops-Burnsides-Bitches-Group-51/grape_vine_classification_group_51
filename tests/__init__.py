4771dab772755a0af07349215e4efae524546095
karluc <s204354@dtu.dk>
2026-01-16 10:25:23 +0100

    Importing correct yaml file in tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,6 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 PATH_DATA = PATH_DATA = PROJECT_ROOT / "data"
 PATH_MODEL = PROJECT_ROOT / "model"
 
-model_config_path = Path("configs/exp1.yaml")
+model_config_path = Path("configs/test.yaml")
 with open(model_config_path, 'r') as f:
     model_config = yaml.safe_load(f)
