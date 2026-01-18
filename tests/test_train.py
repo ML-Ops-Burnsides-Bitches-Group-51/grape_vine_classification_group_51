f44bd4cc86149d88b80ef2418bd79d0a9fb7d704
ClaraHol <s214722@dtu.dk>
2026-01-18 14:41:27 +0100

    Fixed tests so changes can be pushed to git

--- tests/test_train.py
+++ tests/test_train.py
@@ -6,11 +6,7 @@ import pytest
 
 data_path = "data/processed_dataset/"
 
-@pytest.mark.skipif(
-    not os.path.exists(data_path),
-    reason="Training data not available"
-)
-
+@pytest.mark.skipif( not os.path.exists(data_path),reason="Training data not available")
 def test_train():
     train(model_config, output_model_name = "test_model.pth", logger = False)
 
