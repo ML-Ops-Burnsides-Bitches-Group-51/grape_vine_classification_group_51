ec6e97155ae4527b9254531ee03065eb3a9e53f4
karluc <s204354@dtu.dk>
2026-01-18 21:59:22 +0100

    added checks and tests of validity of provided config file

--- tests/test_raises.py
+++ tests/test_raises.py
@@ -15,11 +15,11 @@ def test_error_on_wrong_shape():
         model(torch.rand(1,1,128,129)) 
 
 @timeout_decorator.timeout(10) # time out after a 10 seconds incase RuntimeError is not raised
-def call_train_main(config_path):
-    train_main(config_path = config_path)
+def call_train_main(config_path: str = "", config: dict = None):
+    train_main(config_path = config_path, config = config)
 
 @timeout_decorator.timeout(10) # time out after a 10 seconds incase RuntimeError is not raised
-def call_sweep_main(config_path):
+def call_sweep_main(config_path: str):
     sweep_main(config_path = config_path)
 
 def test_error_on_invalid_config_path():
@@ -28,3 +28,21 @@ def test_error_on_invalid_config_path():
         call_train_main(dummy_path)
     with pytest.raises(RuntimeError, match = "The config path is not valid"):
         call_sweep_main(dummy_path)
+
+def test_validify_config():
+    bad_config = model_config.copy()
+    # if mandatory hyperparameter is not in config
+    mandatory_hyperparameters = ["lr", "epochs", "patience", "optim"]
+    for param in mandatory_hyperparameters:
+        param_val = bad_config.pop(param)
+        with pytest.raises(ValueError, match = f"Config does not contain: {param}"):
+            call_train_main(config = bad_config)
+        bad_config[param] = param_val
+    # if optimzer is not supported
+    bad_config["optim"] = "invalid_optim"
+    with pytest.raises(ValueError, match = "Specified optim is not supported: invalid_optim"):
+        call_train_main(config = bad_config)
+    # if optim = SGD but no momentum was specified
+    bad_config["optim"] = "SGD"
+    with pytest.raises(ValueError, match = "Optim set to SGD, but config does not contain momentum"):
+        call_train_main(config = bad_config)
