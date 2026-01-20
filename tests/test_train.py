58fef541c28ad37d1f16458a9bbc72c9716d259d
karluc <s204354@dtu.dk>
2026-01-20 14:58:03 +0100

    updated input format for training files

--- tests/test_train.py
+++ tests/test_train.py
@@ -8,6 +8,6 @@ data_path = "data/processed_dataset/"
 
 @pytest.mark.skipif(not os.path.exists(data_path), reason="Training data not available")
 def test_train():
-    train(model_config, output_model_name="test_model.pth", logger=False)
+    train(model_config, model_path = PATH_MODEL / "test_model.pth", logger=False)
 
     assert os.path.isfile(PATH_MODEL / "test_model.pth"), "No model saved"
