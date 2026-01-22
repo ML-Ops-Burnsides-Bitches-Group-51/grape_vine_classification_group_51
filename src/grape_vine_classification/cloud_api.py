336e139d8d2bf7d908b5ec9935f03652ff105d43
karluc <s204354@dtu.dk>
2026-01-22 14:43:09 +0100

    cleaned code and added comments

--- src/grape_vine_classification/cloud_api.py
+++ src/grape_vine_classification/cloud_api.py
@@ -10,7 +10,6 @@ import io
 import datetime
 import json
 
-
 BUCKET_NAME = "models_grape_gang"
 MODEL_FILE = "cloud_model.pth"
 LOCAL_MODEL_PATH = "/tmp/model.pth"
@@ -29,18 +28,19 @@ transform = transforms.Compose(
 class PredictionOutput(BaseModel):
     species: str
 
-
-def save_prediction_to_gcp(img: torch.Tensor, outputs: list[float], species: str):
+def save_prediction_to_gcp(img: torch.Tensor, outputs: list[float], species: str) -> None:
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
     
     time = datetime.datetime.now(tz=datetime.UTC)
 
+    # extract features from transformed input image
     avg_brightness = torch.mean(img).item()    
     contrast = torch.std(img).item()
     gradients = torch.gradient(img, dim=[2,3])
     sharpness = torch.mean(torch.abs(gradients[0]) + torch.abs(gradients[1])).item()
 
+    # construct data to be saved
     data = {  
         "avg_brightness": avg_brightness,  
         "contrast": contrast,   
@@ -50,6 +50,7 @@ def save_prediction_to_gcp(img: torch.Tensor, outputs: list[float], species: str
         "species": species,
     }
 
+    # save data to cloud
     blob = bucket.blob(f"predictions/prediction_{time}.json")
     blob.upload_from_string(json.dumps(data))
     print("Finished saving image features and model outputs")
@@ -61,30 +62,27 @@ def download_model():
 
     blob.download_to_filename(LOCAL_MODEL_PATH)
 
-    print("Model downloaded complected succefully: ", os.path.isfile(LOCAL_MODEL_PATH))
+    print("Model downloaded complete and available at: ", os.path.isfile(LOCAL_MODEL_PATH))
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global model
     download_model()
-
-    model = torch.load(LOCAL_MODEL_PATH,map_location=device)
-
-    model.to(device)
+    model = torch.load(LOCAL_MODEL_PATH, map_location = device)
     model.eval()
+    print("model succefully loaded")
 
-    print("model load succefull")
     yield
+
     del model
 
 app = FastAPI(lifespan=lifespan)
 
-@app.post("/predict", response_model=PredictionOutput)
+@app.post("/predict", response_model = PredictionOutput)
 async def predict_species(background_tasks: BackgroundTasks, file: UploadFile = File()):
     
     try:
-
         contents = await file.read()
         image = Image.open(io.BytesIO(contents)).convert("RGB")
         input_tensor = transform(image)
@@ -93,15 +91,15 @@ async def predict_species(background_tasks: BackgroundTasks, file: UploadFile = 
         if input_tensor.ndimension() == 3:
             input_tensor = input_tensor.unsqueeze(0)
 
-
         # Inference
         with torch.no_grad():
-            output = model(input_tensor)
-            prediction = torch.argmax(output, dim=1)
+            outputs = model(input_tensor)
+            prediction = torch.argmax(outputs, dim=1)
             species = class_names[prediction]
 
-        background_tasks.add_task(save_prediction_to_gcp, input_tensor, output.softmax(-1).squeeze().tolist(), species)
-        return PredictionOutput(species=species)
+        background_tasks.add_task(save_prediction_to_gcp, input_tensor, outputs.softmax(-1).squeeze().tolist(), species)
+
+        return PredictionOutput(species = species)
 
     except Exception as e:
         return HTTPException(status_code=500, detail=str(e))
